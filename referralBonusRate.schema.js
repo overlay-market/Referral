56756969066c2ccf240965c31f734de47a1c5143
c-note <dongustavo20@gmail.com>
2023-04-05 09:38:21 +0100

    updated schema

--- referralBonusRate.schema.js
+++ referralBonusRate.schema.js
@@ -1,12 +1,36 @@
 const mongoose = require("mongoose");
 
 const referralBonusRateSchema = new mongoose.Schema({
-  lowerBound: {
-    type: Number,
+  RBR: {
+    type: String,
     required: true,
   },
   rate: {
+    type: Array,
+    required: true,
+  },
+  decimal: {
     type: Number,
+    required: true,
+  },
+  levelRate: {
+    type: Array,
+    required: true,
+  },
+  lowerBound: {
+    type: Array,
+    required: true,
+  },
+  referralBonus: {
+    type: Number,
+    required: true,
+  },
+  secondsUntilInactive: {
+    type: Number,
+    required: true,
+  },
+  onlyRewardActiveReferrers: {
+    type: Boolean,
     required: true,
   },
 });
