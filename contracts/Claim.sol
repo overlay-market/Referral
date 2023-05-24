649fb7f19faa1c0d263637adbcba1c5dd4a54e2a
c-note <dongustavo20@gmail.com>
2023-05-24 11:07:16 +0100

    added prettier

--- contracts/Claim.sol
+++ contracts/Claim.sol
@@ -4,6 +4,7 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
+error Claim_Only_Guardian();
 error Claim_Invalid_Signature();
 error Claim_Must_Have_Admin_Role();
 error Claim_Amount_Must_Be_Above_0();
@@ -16,19 +17,38 @@ contract Claim is AccessControlEnumerable {
 
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
+    bytes32 public constant GUARDIAN_ROLE = keccak256("GUARDIAN_ROLE");
 
     constructor(address _tokenAddress) {
         token = IERC20(_tokenAddress);
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function grantNewRole(address account, bytes32 _role) external {
-        if(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) revert Claim_Must_Have_Admin_Role();
+    modifier onlyGuardian() {
+        if (!hasRole(GUARDIAN_ROLE, msg.sender)) revert Claim_Only_Guardian();
+        _;
+    }
+
+    modifier onlyAdmin() {
+        if (!hasRole(DEFAULT_ADMIN_ROLE, msg.sender))
+            revert Claim_Must_Have_Admin_Role();
+        _;
+    }
+
+    modifier onlyWhenContractHasEnoughTokens(uint256 _amount) {
+        if (token.balanceOf(address(this)) < _amount)
+            revert Claim_Contract_Does_Not_Have_Enough_Tokens();
+        _;
+    }
+
+    function grantNewRole(address account, bytes32 _role) external onlyAdmin {
         grantRole(_role, account);
     }
 
-    function revokeExistingRole(address account, bytes32 _role) external {
-        if(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) revert Claim_Must_Have_Admin_Role();
+    function revokeExistingRole(
+        address account,
+        bytes32 _role
+    ) external onlyAdmin {
         revokeRole(_role, account);
     }
 
@@ -37,23 +57,23 @@ contract Claim is AccessControlEnumerable {
         address _recipient,
         bytes32 _messageHash,
         bytes memory _signature
-    ) external {
-        // TODO 
+    ) external onlyWhenContractHasEnoughTokens(_amount) {
+        // TODO
         // Check the max amount to send for a day hasn't been reached
         bytes32 messageHash = _messageHash.toEthSignedMessageHash();
-
-        if(_amount == 0) revert Claim_Amount_Must_Be_Above_0();
-        if(token.balanceOf(address(this)) < _amount) revert Claim_Contract_Does_Not_Have_Enough_Tokens();
+        if (_amount == 0) revert Claim_Amount_Must_Be_Above_0();
 
         // Verify the signature
         address signer = messageHash.recover(_signature);
-        if(!hasRole(SIGNER_ROLE, signer)) revert Claim_Invalid_Signature();
+        if (!hasRole(SIGNER_ROLE, signer)) revert Claim_Invalid_Signature();
 
         token.transfer(_recipient, _amount);
     }
 
-    function withdrawToken(address _recipient, uint256 _amount ) external {
-        if(token.balanceOf(address(this)) < _amount) revert Claim_Contract_Does_Not_Have_Enough_Tokens();
+    function withdrawToken(
+        address _recipient,
+        uint256 _amount
+    ) external onlyGuardian onlyWhenContractHasEnoughTokens(_amount) {
         token.transfer(_recipient, _amount);
     }
 }
--- contracts/HelpfulInterface.sol
+++ contracts/HelpfulInterface.sol
@@ -2,20 +2,31 @@
 pragma solidity ^0.8.9;
 
 interface HelpfulInterface {
-    function build(uint256 collateral,
+    function build(
+        uint256 collateral,
         uint256 leverage,
         bool isLong,
-        uint256 priceLimit) external;
+        uint256 priceLimit
+    ) external;
 
     function factory() external view returns (address);
+
     function params(uint256 idx) external view returns (uint256);
 
-     function approve(address spender, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
 
-     function notional(address _address, address sender, uint id) external view returns (uint);
-     function transfer(address recipient, uint256 amount) external returns (bool);
+    function notional(
+        address _address,
+        address sender,
+        uint id
+    ) external view returns (uint);
 
-     event Build(
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Build(
         address indexed sender, // address that initiated build (owns position)
         uint256 positionId, // id of built position
         uint256 oi, // oi of position at build
