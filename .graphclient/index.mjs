54c0c443ed65b01ba0af30d3a892c35ba54abe0c
TomasCImach <tomas90@gmail.com>
2023-06-06 16:03:03 -0300

    add subgraph query client

---
+++ .graphclient/index.mjs
@@ -0,0 +1,117 @@
+import { PubSub } from '@graphql-mesh/utils';
+import { DefaultLogger } from '@graphql-mesh/utils';
+import MeshCache from "@graphql-mesh/cache-localforage";
+import { fetch as fetchFn } from '@whatwg-node/fetch';
+import GraphqlHandler from "@graphql-mesh/graphql";
+import AutoPaginationTransform from "@graphprotocol/client-auto-pagination";
+import BareMerger from "@graphql-mesh/merger-bare";
+import { createMeshHTTPHandler } from '@graphql-mesh/http';
+import { getMesh } from '@graphql-mesh/runtime';
+import { MeshStore, FsStoreStorageAdapter } from '@graphql-mesh/store';
+import { path as pathModule } from '@graphql-mesh/cross-helpers';
+import * as importedModule$0 from "./sources/overlayv2/introspectionSchema.json";
+import { fileURLToPath } from '@graphql-mesh/utils';
+const baseDir = pathModule.join(pathModule.dirname(fileURLToPath(import.meta.url)), '..');
+const importFn = (moduleId) => {
+    const relativeModuleId = (pathModule.isAbsolute(moduleId) ? pathModule.relative(baseDir, moduleId) : moduleId).split('\\').join('/').replace(baseDir + '/', '');
+    switch (relativeModuleId) {
+        case ".graphclient/sources/overlayv2/introspectionSchema.json":
+            return Promise.resolve(importedModule$0);
+        default:
+            return Promise.reject(new Error(`Cannot find module '${relativeModuleId}'.`));
+    }
+};
+const rootStore = new MeshStore('.graphclient', new FsStoreStorageAdapter({
+    cwd: baseDir,
+    importFn,
+    fileType: "json",
+}), {
+    readonly: true,
+    validate: false
+});
+export const rawServeConfig = undefined;
+export async function getMeshOptions() {
+    const pubsub = new PubSub();
+    const sourcesStore = rootStore.child('sources');
+    const logger = new DefaultLogger("GraphClient");
+    const cache = new MeshCache({
+        ...{},
+        importFn,
+        store: rootStore.child('cache'),
+        pubsub,
+        logger,
+    });
+    const sources = [];
+    const transforms = [];
+    const additionalEnvelopPlugins = [];
+    const overlayv2Transforms = [];
+    const additionalTypeDefs = [];
+    const overlayv2Handler = new GraphqlHandler({
+        name: "overlayv2",
+        config: { "endpoint": "https://api.studio.thegraph.com/query/46086/overlay-v2-subgraph-arbitrum/version/latest" },
+        baseDir,
+        cache,
+        pubsub,
+        store: sourcesStore.child("overlayv2"),
+        logger: logger.child("overlayv2"),
+        importFn,
+    });
+    overlayv2Transforms[0] = new AutoPaginationTransform({
+        apiName: "overlayv2",
+        config: { "validateSchema": true, "limitOfRecords": 1000 },
+        baseDir,
+        cache,
+        pubsub,
+        importFn,
+        logger,
+    });
+    sources[0] = {
+        name: 'overlayv2',
+        handler: overlayv2Handler,
+        transforms: overlayv2Transforms
+    };
+    const additionalResolvers = [];
+    const merger = new BareMerger({
+        cache,
+        pubsub,
+        logger: logger.child('bareMerger'),
+        store: rootStore.child('bareMerger')
+    });
+    return {
+        sources,
+        transforms,
+        additionalTypeDefs,
+        additionalResolvers,
+        cache,
+        pubsub,
+        merger,
+        logger,
+        additionalEnvelopPlugins,
+        get documents() {
+            return [];
+        },
+        fetchFn,
+    };
+}
+export function createBuiltMeshHTTPHandler() {
+    return createMeshHTTPHandler({
+        baseDir,
+        getBuiltMesh: getBuiltGraphClient,
+        rawServeConfig: undefined,
+    });
+}
+let meshInstance$;
+export function getBuiltGraphClient() {
+    if (meshInstance$ == null) {
+        meshInstance$ = getMeshOptions().then(meshOptions => getMesh(meshOptions)).then(mesh => {
+            const id = mesh.pubsub.subscribe('destroy', () => {
+                meshInstance$ = undefined;
+                mesh.pubsub.unsubscribe(id);
+            });
+            return mesh;
+        });
+    }
+    return meshInstance$;
+}
+export const execute = (...args) => getBuiltGraphClient().then(({ execute }) => execute(...args));
+export const subscribe = (...args) => getBuiltGraphClient().then(({ subscribe }) => subscribe(...args));
