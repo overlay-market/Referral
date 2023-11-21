ddb20d15060fd3df6abd42de5ee38b407252e473
Erik <esnetto@gmail.com>
2023-11-21 18:28:06 +0000

    Delete unnecessary files and configurations

--- README.md
+++
@@ -1,4 +0,0 @@
-# Referral
-
-Set up your env file by using `.env.example`
-Then run `npm run test`
--- src/api.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const referralsRouter = require("./referrals.router.js");
-
-const api = express.Router();
-api.use("/referral", referralsRouter);
-
-module.exports = api;
