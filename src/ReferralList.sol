63cb90c6877dfd81cbb2af3f0dee47f508ed2388
Erik <esnetto@gmail.com>
2023-11-21 19:54:41 +0000

    Add Ownable Extension

--- src/ReferralList.sol
+++ src/ReferralList.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ReferralList {
+import {Ownable} from "solady/src/auth/OwnableRoles.sol";
+
+contract ReferralList is Ownable {
     error ReferredAlreadyExists();
     error ReferrerNotAllowed();
 
     mapping(address referred => address referrer) public referrals;
     mapping(address referrer => bool isAllowed) public allowedReferrers;
+
+    constructor() {
+        _initializeOwner(msg.sender);
+    }
 
     function addReferrer(address _referrer) public {
         if (referrals[_referrer] != address(0)) revert ReferredAlreadyExists();
