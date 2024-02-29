171f18e1355435ab088423d4c5df6bcd2df984be
Ezequiel <eperez@itba.edu.ar>
2024-02-29 17:00:38 -0300

    chore: remove server, moved to rewards-api

--- server/src/signatures/signatures.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common"
-import { SignaturesController } from "./signatures.controller"
-import { SignaturesService } from "./signatures.service"
-
-@Module({
-    controllers: [SignaturesController],
-    providers: [SignaturesService],
-})
-export class SignaturesModule {}
