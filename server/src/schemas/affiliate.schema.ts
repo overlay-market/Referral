ed10580cb8d1fe5774538661b665043542d4c543
Arthur G. <me@arthurka.eu>
2024-12-10 16:29:04 +0100

    fix: add lowercase for schemes

--- server/src/schemas/affiliate.schema.ts
+++ server/src/schemas/affiliate.schema.ts
@@ -3,7 +3,7 @@ import { Document } from "mongoose"
 
 @Schema()
 export class Affiliate extends Document {
-    @Prop({ required: true, unique: true })
+    @Prop({ required: true, unique: true, lowercase: true })
     address: string
 
     @Prop()
